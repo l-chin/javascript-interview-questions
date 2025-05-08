8e8cf5dcfeb8600c98ea4256dfa36a985e37a8da
l_qin <122345830+l-chin@users.noreply.github.com>
2025-05-08 22:32:41 +0800

    update

--- leetcode/AddTwoNumber.ts
+++ leetcode/AddTwoNumber.ts
@@ -42,3 +42,43 @@ function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | nul
     // Return the result list (skip the dummy head)
     return dummyHead.next;
 }
+
+const simpleAddTwoNumbers = (l1: ListNode | null, l2: ListNode | null): ListNode | null => {
+  let resultHead = new ListNode(0);
+  console.log('resultHead', resultHead)
+  let current = resultHead
+  console.log('current', current)
+  let carry = 0;
+  while (l1 !== null || l2 !== null || carry !== 0) {
+    console.log('l1', l1)
+    console.log('l2', l2)
+    let v1 = l1 ? l1.val : 0;
+    let v2 = l2 ? l2.val : 0
+
+    let sum = v1 + v2 + carry
+    carry = Math.floor(sum / 10)
+    let digit = sum % 10
+    
+    current.next = new ListNode(digit)
+    console.log('current', current)
+    current = current.next
+    console.log('current', current)
+
+    if(l1) {
+      l1 = l1.next
+      console.log('l1', l1)
+    }
+    if(l2) {
+      l2 = l2.next
+      console.log('l2', l2)
+    }
+    console.log('resultHead', resultHead)
+    console.log('-----------------------')
+  }
+  console.log('resultHead.next', resultHead.next)
+  return resultHead.next
+
+}
+let l1 = new ListNode(2, new ListNode(4, new ListNode(3)))
+let l2 = new ListNode(5, new ListNode(6, new ListNode(4)))
+simpleAddTwoNumbers(l1, l2)
---
+++ leetcode/longestSubstrWithoutRepeat.ts
@@ -0,0 +1,45 @@
+// Given a string s, find the length of the longest substring without duplicate characters.
+
+ 
+
+// Example 1:
+
+// Input: s = "abcabcbb"
+// Output: 3
+// Explanation: The answer is "abc", with the length of 3.
+// Example 2:
+
+// Input: s = "bbbbb"
+// Output: 1
+// Explanation: The answer is "b", with the length of 1.
+// Example 3:
+
+// Input: s = "pwwkew"
+// Output: 3
+// Explanation: The answer is "wke", with the length of 3.
+// Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+ 
+
+// Constraints:
+
+// 0 <= s.length <= 5 * 104
+// s consists of English letters, digits, symbols and spaces.
+
+function lswr(s: string) {
+  let maxLength = 0;
+  let left = 0;
+  const charMap = new Map();
+
+  for(let right = 0; right < s.length; right++) {
+    const currentChar = s[right];
+
+    if(charMap.has(currentChar) && charMap.get(currentChar) >= left) {
+      left = charMap.get(currentChar) + 1;
+    }
+
+    charMap.set(currentChar, right);
+    maxLength = Math.max(maxLength, right - left + 1);
+  }
+
+  return maxLength;
+}
