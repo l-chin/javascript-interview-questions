bc8e6c07c926774d03b4deb31e55259aef552836
l_qin <122345830+l-chin@users.noreply.github.com>
2025-05-12 23:25:09 +0800

    update

--- leetcode/longestPalindromic.ts
+++ leetcode/longestPalindromic.ts
@@ -12,25 +12,50 @@
 // 1 <= s.length <= 1000
 // s consist of only digits and English letters.
 
-const longestPalindromic = (s: string) => {
+// const longestPalindromic = (s: string) => {
 
-  if(s.length < 3) return s;
+//   if(s.length < 3) return s;
+//   let maxLen = 1;
+//   let start = 0
+
+//   for (let i = 0; i < s.length - 1; i++) {
+//    for(let j = i+1; j < s.length; j++) {
+//     const subStr = s.slice(i, j)
+//     if (isPalindrome(subStr) && subStr.length > maxLen) {
+//      maxLen = subStr.length;
+//      start = i
+//     }
+//    }
+//   }
+//   return s.slice(start, start + maxLen)
+
+// }
+
+// const isPalindrome = (s: string) => {
+//   return s === s.split('').reverse().join('');
+// }
+
+function longestPalindromic(s:string) {
+  if(s.length < 2) return s
   let maxLen = 1;
-  let start = 0
+  let start = 0;
 
-  for (let i = 0; i < s.length - 1; i++) {
-   for(let j = i+1; j < s.length; j++) {
-    const subStr = s.slice(i, j)
-    if (isPalindrome(subStr) && subStr.length > maxLen) {
-     maxLen = subStr.length;
-     start = i
+  const expand = (left:number, right:number) => {
+    while (left >= 0 && right < s.length && s[left] === s[right]) {
+      left--;
+      right++;
     }
-   }
+    return right - left - 1;
+  }
+
+  for(let i = 0; i < s.length; i++) {
+    let len1 = expand(i, i)
+    let len2 = expand(i, i+ 1)
+    let len = Math.max(len1, len2)
+    if(len > maxLen) {
+      maxLen = len;
+      start = i - Math.floor((len - 1) / 2)
+    }
   }
   return s.slice(start, start + maxLen)
-
-}
-
-const isPalindrome = (s: string) => {
-  return s === s.split('').reverse().join('');
 }
